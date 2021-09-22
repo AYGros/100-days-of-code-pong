987d1bd8df58f02f2bf3ac59ae524263ee5751ea
yuki <mansie.torsholt@gmail.com>
2021-09-22 23:37:34 +0200

    make scoreboard and speed up ball

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import time
 from turtle import Screen
 from paddle import Paddle
 from ball import Ball
+from scoreboard import Scoreboard
 #game setup
 screen = Screen()
 screen.bgcolor("black")
@@ -10,6 +11,8 @@ screen.title("Pong")
 screen.tracer(0)
 
 screen.listen()
+
+scoreboard = Scoreboard()
 
 r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
@@ -20,24 +23,28 @@ screen.onkey(r_paddle.down, "Down")
 screen.onkey(l_paddle.up, "w")
 screen.onkey(l_paddle.down, "s")
 
-#actual game functionality
+# actual game functionality
 game_is_on = True
 
 while game_is_on:
     screen.update()
-    time.sleep(0.1)
+    time.sleep(ball.ball_speed)
     ball.move()
-    #detect wall collision
+    # detect wall collision
     if ball.ycor() > 280 or ball.ycor() < -280:
         ball.bounce_y()
-    #detect right paddle collision
+    # detect right or left paddle collision
     if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
         ball.bounce_x()
-    #detect paddle miss and back wall hit
+
+    # detect right paddle miss and back wall hit
     if ball.xcor() > 360:
         ball.restart()
+        scoreboard.l_point()
 
+    # detect left paddle miss and back wall hit
     if ball.xcor() < -360:
         ball.restart()
+        scoreboard.r_point()
 
 screen.exitonclick()
